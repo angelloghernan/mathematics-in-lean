4bb115dfa418e230ea0183a39726f1ef0fc97e4c
Gianni Hernandez <a.gianni1234@gmail.com>
2024-03-18 17:09:28 -0700

    C05/S01

--- MIL/C05_Elementary_Number_Theory/S01_Irrational_Roots.lean
+++ MIL/C05_Elementary_Number_Theory/S01_Irrational_Roots.lean
@@ -139,28 +139,42 @@ example {m n p : ℕ} (nnz : n ≠ 0) (prime_p : p.Prime) : m ^ 2 ≠ p * n ^ 2 
   intro sqr_eq
   have nsqr_nez : n ^ 2 ≠ 0 := by simpa
   have eq1 : Nat.factorization (m ^ 2) p = 2 * m.factorization p := by
-    sorry
+    exact factorization_pow' m 2 p
   have eq2 : (p * n ^ 2).factorization p = 2 * n.factorization p + 1 := by
-    sorry
+    rw [factorization_mul']
+    rw [Nat.Prime.factorization']
+    rw [factorization_pow']
+    . ring
+    . exact prime_p
+    . exact Nat.Prime.ne_zero prime_p
+    . exact nsqr_nez
   have : 2 * m.factorization p % 2 = (2 * n.factorization p + 1) % 2 := by
     rw [← eq1, sqr_eq, eq2]
   rw [add_comm, Nat.add_mul_mod_self_left, Nat.mul_mod_right] at this
   norm_num at this
 
-example {m n k r : ℕ} (nnz : n ≠ 0) (pow_eq : m ^ k = r * n ^ k) {p : ℕ} (prime_p : p.Prime) :
+example {m n k r : ℕ} (nnz : n ≠ 0) (pow_eq : m ^ k = r * n ^ k) {p : ℕ} (_ : p.Prime) :
     k ∣ r.factorization p := by
   rcases r with _ | r
   · simp
-  have npow_nz : n ^ k ≠ 0 := fun npowz ↦ nnz (pow_eq_zero npowz)
+  have npow_nz : n ^ k ≠ 0 := by 
+    intro npowz
+    apply nnz (pow_eq_zero npowz)
   have eq1 : (m ^ k).factorization p = k * m.factorization p := by
-    sorry
+    exact factorization_pow' m k p
   have eq2 : (r.succ * n ^ k).factorization p =
       k * n.factorization p + r.succ.factorization p := by
-    sorry
+      rw [← factorization_pow']
+      rw [← factorization_mul']
+      rw [mul_comm]
+      . exact npow_nz
+      . exact r.succ_ne_zero
   have : r.succ.factorization p = k * m.factorization p - k * n.factorization p := by
     rw [← eq1, pow_eq, eq2, add_comm, Nat.add_sub_cancel]
   rw [this]
-  sorry
+  apply Nat.dvd_sub'
+  . apply Nat.dvd_mul_right
+  . apply Nat.dvd_mul_right
 
 #check multiplicity
 
